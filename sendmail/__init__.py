fd78a04961ed5487db04dd939f3879696dc02552
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2017-02-17 16:36:54 +1100

    Initial commit

---
+++ sendmail/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'mail.synchrotron.org.au'
+app.config['MAIL_DEFAULT_SENDER'] = 'email_robot@synchrotron.org.au'
+app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['TESTING'] = False
+mail = Mail(app)
+
+import sendmail.views
---
+++ sendmail/views.py
@@ -0,0 +1,53 @@
+from sendmail import app,mail
+from flask_mail import Message
+from flask import request
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+st = logging.StreamHandler()
+st.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s [%(name)s] %(levelname)s :: %(message)s")
+st.setFormatter(formatter)
+logger.addHandler(st)
+from jinja2 import Template
+
+
+
+@app.route('/')
+def index():
+    return Template("<link rel='shortcut icon' href='/static/img/favicon.png' >" +
+                    "<pre>curl --data 'subject=&lt;subject&gt;&body=&lt;body&gt;" +
+                    "&recipients=&lt;recipient[@synchrotron.org.au]&gt;[,one][,two][,etc...]' " +
+                    "hostAddress:Port/sendmail/</pre>").render()
+
+@app.route('/sendmail/', methods=['POST'])
+def post_the_mail():
+    # required to send message
+    # subject<string>, body<string>, recipients<list>
+    subject_string = str(request.form.get('subject'))
+    body_string = str(request.form.get('body'))
+    tmp_list = str(request.form.get('recipients')).split(',')
+
+    recipient_list = []
+    for recipient in tmp_list:
+        r = recipient
+        if '@' not in recipient:
+            r = r + '@synchrotron.org.au'
+        recipient_list.append(r)
+
+    logger.info("%s, %s, %s" % (subject_string, body_string, tmp_list))
+
+    with mail.connect() as con:
+        logger.info('Sending Email Message to the following users: ')
+        for user in recipient_list:
+            logger.info(user)
+            msg = Message(subject=subject_string,
+                          html=body_string,
+                          recipients=[user])
+            con.send(msg)
+
+    logger.info("Email Sent, you don't have anymore messages!")
+    return "Done!"
